a0fed4c33ac984d52cacb3c35a599083ebf6ad41
DosKobold <hanspeter56@t-online.de>
2024-05-11 15:20:44 +0200

    graphics: added basic methods

--- source/view/graphics.java
+++ source/view/graphics.java
@@ -1,6 +1,8 @@
 /*TODO:
- * Tom wegen Zeitplan fragen
- * Tom wegen Implementierung/Interface fragen
+ * Return 2D ArrayList of spawns at loadMap(String)
+ * Troop drawing
+ * Scaling with the correct ratio
+ * Design, especially the placement of text output
  */
 
 //package view;
@@ -13,17 +15,41 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.*;
 import javafx.scene.canvas.*;
 import javafx.scene.image.*;
+import javafx.scene.text.Font;
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class graphics extends Application {
 	
+	int windowX = 1000;
+	int windowY =  900;
+
+	//Window objects
+	Stage primaryStage;
+	Group root = new Group();
+	Scene scene = new Scene(root,windowX,windowY,Color.RED);
+	final Canvas canvas = new Canvas(windowX,windowY);
+	GraphicsContext gc = canvas.getGraphicsContext2D();
+
+
+	//Data
 	private ArrayList<ArrayList<Character>> map = new ArrayList<>();
+	private ArrayList<String> textStrings = new ArrayList<>();
+	private ArrayList<Paint>  textColors  = new ArrayList<>();
+
+
+	//Functions
+	public void graphics(String text) throws Exception {
+		root.getChildren().add(canvas);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("DigitalerVernichtungskrieg");
+		primaryStage.show();
+	}
 
 	//TODO: return spawn?
 	public void loadMap(String fileName) throws Exception {
-		Scanner scanner = new Scanner(new File("../../maps/01_Little_Island/map.csv"));
+		Scanner scanner = new Scanner(new File(fileName));
 		scanner.useDelimiter(",");
 		int lineNo = 0;
 		map.add(new ArrayList());
@@ -39,14 +65,7 @@ public class graphics extends Application {
 		scanner.close();
 	}
 
-	public void start(Stage primaryStage) throws Exception {
-		loadMap("TEst"); //FIXME: ONLY DURING NOT IMPLEMENTED
-		Group root = new Group();
-		Scene scene = new Scene(root,1000,1000,Color.PINK);
-
-		final Canvas canvas = new Canvas(1000,1000);
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		
+	public void drawMap() throws Exception {
 		//Draw background map
 		final Image plain    = new Image("file:../../textures/plain.png");
 		final Image wood     = new Image("file:../../textures/wood.png");	//FIXME: Images should be placed in class, not every time loaded
@@ -61,7 +80,7 @@ public class graphics extends Application {
 						break;
 					case 'W':
 						gc.drawImage(wood, 32*fieldNo, 32*lineNo);
-						break;						//FIXME: 32 should be variable to window size
+						break;	//FIXME: 32 should be variable to window size
 					case 'M':
 						gc.drawImage(mountain, 32*fieldNo, 32*lineNo);
 						break;
@@ -71,16 +90,63 @@ public class graphics extends Application {
 				}
 			}
 		}
+
+		//Draw troops
+		/*TODO: implement drawing troops
+		 *	under construction
+		*/
 		
-		root.getChildren().add(canvas);
-	
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("DigitalerVernichtungskrieg");
-		primaryStage.show();
+		//Draw text
+		gc.setFont(new Font(20));
+		for (int pos=0; pos<textStrings.size(); pos++) {
+			switch (pos) {
+				case 0:
+					gc.setFill(textColors.get(pos));
+					gc.fillText(textStrings.get(pos),10,20);
+				case 1:
+					gc.setFill(textColors.get(pos));
+					gc.fillText(textStrings.get(pos),windowX-10,20);
+				case 2:
+					gc.setFill(textColors.get(pos));
+					gc.fillText(textStrings.get(pos),windowX/2,windowY/2);
+				case 3:
+					gc.setFill(textColors.get(pos));
+					gc.fillText(textStrings.get(pos),10,windowY-20);
+				case 4:
+					gc.setFill(textColors.get(pos));
+					gc.fillText(textStrings.get(pos),windowX-10,windowY-20);
+			}
+		}
+	}
+
+	public void setText(int pos, String text, Paint color) {
+		textStrings.add(pos, text);
+		textColors.add(pos, color);
+	}
+
+	public void removeText(int pos) {
+		textStrings.remove(pos);
+		textColors.remove(pos);
+	}
+
+	public char getTerrain(int x, int y) {
+		return map.get(y).get(x);
+	}
+
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		graphics("THIS IS A TEXT");
+		loadMap("../../maps/17_Piston_Dam/map.csv"); //FIXME: ONLY DURING NOT IMPLEMENTED
+		
+		setText(0, "THIS IS A TEST", Color.BLACK);
+		setText(1, "THIS IS A TEST", Color.BLACK);
+		setText(2, "THIS IS A TEST", Color.BLACK);
+		setText(3, "THIS IS A TEST", Color.BLACK);
+		setText(4, "THIS IS A TEST", Color.BLACK);
+		drawMap();
 	}
 
 	public static void main (String[] args) {
 		launch(args);
 	}
-
 }
