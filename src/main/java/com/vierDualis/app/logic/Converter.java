c396acfdb75195fa3c38f755361016e378b97c95
Tom Schwindl <schwindl@posteo.de>
2024-07-12 20:29:22 +0200

    add troop converter

---
+++ src/main/java/com/vierDualis/app/logic/Converter.java
@@ -0,0 +1,67 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Converter {
+	public ArrayList<Troop> convertTroops(ArrayList<ArrayList<Character>> troops2d)
+	{
+		ArrayList<Troop> troops = new ArrayList<>();
+
+		for (int line = 0; i < troops2d.size(); ++i) {
+			for (int field = 0; field < troops2d.get(line); ++field) {
+				switch (troops2d.get(line).get(field)) {
+				case 'I':
+					troops.add(new Infantry(field, line, "red"));
+					break;
+				case 'i':
+					troops.add(new Infantry(field, line, "blue"));
+					break;
+				case 'M':
+					troops.add(new MechanizedInfantry(field, line, "red"));
+					break;
+				case 'm':
+					troops.add(new MechanizedInfantry(field, line, "blue"));
+					break;
+				case 'T':
+					troops.add(new Tank(field, line, "red"));
+					break;
+				case 't':
+					troops.add(new Tank(field, line, "blue"));
+					break;
+				case 'D':
+					troops.add(new MobileArtillery(field, line, "red"));
+					break;
+				case 'd':
+					troops.add(new MobileArtillery(field, line, "blue"));
+					break;
+				case 'A':
+					troops.add(new AntiAir(field, line, "red"));
+					break;
+				case 'a':
+					troops.add(new AntiAir(field, line, "blue"));
+					break;
+				case 'F':
+					troops.add(new Fighter(field, line, "red"));
+					break;
+				case 'f':
+					troops.add(new Fighter(field, line, "blue"));
+					break;
+				case 'B':
+					troops.add(new Bomber(field, line, "red"));
+					break;
+				case 'b':
+					troops.add(new Bomber(field, line, "blue"));
+					break;
+				case 'C':
+					troops.add(new BattleCopter(field, line, "red"));
+					break;
+				case 'c':
+					troops.add(new BattleCopter(field, line, "blue"));
+					break;
+				}
+			}
+		}
+
+		return troops;
+	}
+}
